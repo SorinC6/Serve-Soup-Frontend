9bc84eb893e95771bc02f1c4537c23516d390b1a
Sorin Chis <kish.sorin@yahoo.com>
2019-05-20 15:37:49 +0300

    added kitchen reducer

---
+++ kitchen/src/store/reducers/kitchensReducer.js
@@ -0,0 +1,26 @@
+import * as types from "../actions/actionTypes";
+
+const initialState = {
+  kitchenData: [],
+  error: null,
+  loading: true
+};
+
+export const allKitchen = (state = initialState, action) => {
+  switch (action.tyle) {
+    case types.GET_SOUP_KITCHENS_START:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case types.GET_SOUP_KITCHENS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        kitchenData: action.payload
+      };
+    default:
+      return state;
+  }
+};
