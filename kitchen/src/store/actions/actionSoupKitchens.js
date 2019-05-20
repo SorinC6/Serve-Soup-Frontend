14cfda7e3f9da0651e59644feab43de550c8c3e8
Sorin Chis <kish.sorin@yahoo.com>
2019-05-20 15:01:40 +0300

    added actions types for getting soup kitchens

---
+++ kitchen/src/store/actions/actionSoupKitchens.js
@@ -0,0 +1,8 @@
+import * as types from "./actionTypes";
+import * as url from "../../constants/ApiConstants";
+import axiosWithAuth from "../axios/index";
+
+axiosWithAuth()
+  .get()
+  .then()
+  .catch();
