a685ebc56c29764c1b74bab40a18b39c3c290f6a
Sorin Chis <kish.sorin@yahoo.com>
2019-05-20 15:05:51 +0300

    added action creators for gettin all soup kitchens from database

--- kitchen/src/store/actions/actionSoupKitchens.js
+++ kitchen/src/store/actions/actionSoupKitchens.js
@@ -2,7 +2,17 @@ import * as types from "./actionTypes";
 import * as url from "../../constants/ApiConstants";
 import axiosWithAuth from "../axios/index";
 
-axiosWithAuth()
-  .get()
-  .then()
-  .catch();
+export const getAllSoupKitchens = () => dispatch => {
+  dispatch({ type: types.GET_SOUP_KITCHENS_START });
+
+  axiosWithAuth()
+    .get(`${url.soupKitcheks}`)
+    .then(res => {
+      console.log(res.data);
+      dispatch({ type: types.GET_SOUP_KITCHENS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err.message);
+      dispatch({ type: types.GET_SOUP_KITCHENS_FAIL, payload: err.message });
+    });
+};
